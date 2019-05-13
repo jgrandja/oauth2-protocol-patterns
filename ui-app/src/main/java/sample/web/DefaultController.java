61b9c4894753c86959033e180e0152c42145b398
Joe Grandja <jgrandja@pivotal.io>
2019-05-13 14:48:18 -0400

    Move session state to new view

--- ui-app/src/main/java/sample/web/DefaultController.java
+++ ui-app/src/main/java/sample/web/DefaultController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2018 the original author or authors.
+ * Copyright 2002-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -40,5 +40,10 @@ public class DefaultController {
 		String errorMessage = authEx != null ? authEx.getMessage() : "[unknown error]";
 		model.put("errorMessage", errorMessage);
 		return "error";
+	}
+
+	@GetMapping("/session-state")
+	public String sessionState() {
+		return "session-state";
 	}
 }
--- ui-app/src/main/java/sample/web/DefaultControllerAdvice.java
+++ ui-app/src/main/java/sample/web/DefaultControllerAdvice.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2018 the original author or authors.
+ * Copyright 2002-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,7 +15,14 @@
  */
 package sample.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ResolvableType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -23,14 +30,27 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 /**
  * @author Joe Grandja
  */
 @ControllerAdvice
 public class DefaultControllerAdvice {
+
+	@Autowired
+	private ClientRegistrationRepository clientRegistrationRepository;
+
+	@Autowired
+	private OAuth2AuthorizedClientService authorizedClientService;
 
 	@ModelAttribute("currentUser")
 	UserModel currentUser(@AuthenticationPrincipal OidcUser oidcUser) {
@@ -42,6 +62,40 @@ public class DefaultControllerAdvice {
 			currentUser.setEmail(oidcUser.getEmail());
 		}
 		return currentUser;
+	}
+
+	@ModelAttribute("idTokenClaims")
+	Map<String, Object> idTokenClaims(@AuthenticationPrincipal OidcUser oidcUser) {
+		if (oidcUser == null) {
+			return Collections.emptyMap();
+		}
+		final List<String> claimNames = Arrays.asList("iss", "sub", "aud", "azp", "given_name", "family_name", "email");
+		return oidcUser.getClaims().entrySet().stream()
+				.filter(e -> claimNames.contains(e.getKey()))
+				.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+	}
+
+	@ModelAttribute("authorizedClientRegistrations")
+	List<AuthorizedClientRegistrationModel> authorizedClientRegistrations(OAuth2AuthenticationToken oauth2Authentication) {
+		List<AuthorizedClientRegistrationModel> authorizedClientRegistrations = new ArrayList<>();
+		getClientRegistrations().forEach(registration -> {
+			OAuth2AuthorizedClient authorizedClient = this.authorizedClientService.loadAuthorizedClient(
+					registration.getRegistrationId(), oauth2Authentication.getName());
+			authorizedClientRegistrations.add(
+					new AuthorizedClientRegistrationModel(registration, authorizedClient));
+
+		});
+		authorizedClientRegistrations.sort(Comparator.comparing(AuthorizedClientRegistrationModel::getClientId));
+		return authorizedClientRegistrations;
+	}
+
+	private List<ClientRegistration> getClientRegistrations() {
+		ResolvableType type = ResolvableType.forInstance(this.clientRegistrationRepository).as(Iterable.class);
+		if (type != ResolvableType.NONE && ClientRegistration.class.isAssignableFrom(type.resolveGenerics()[0])) {
+			return StreamSupport.stream(((Iterable<ClientRegistration>) clientRegistrationRepository).spliterator(), false)
+					.collect(Collectors.toList());
+		}
+		return Collections.emptyList();
 	}
 
 	@ExceptionHandler(WebClientResponseException.class)

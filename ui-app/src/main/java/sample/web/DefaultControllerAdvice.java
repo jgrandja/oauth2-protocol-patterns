8d0b0e46989673536912c07561e15a9bbb460f48
Joe Grandja <jgrandja@vmware.com>
2021-01-26 08:38:22 -0500

    Catch WebClientRequestException

--- ui-app/src/main/java/sample/web/DefaultControllerAdvice.java
+++ ui-app/src/main/java/sample/web/DefaultControllerAdvice.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2019 the original author or authors.
+ * Copyright 2002-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,6 +15,16 @@
  */
 package sample.web;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ResolvableType;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
@@ -26,18 +36,9 @@ import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.reactive.function.client.WebClientRequestException;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
 /**
  * @author Joe Grandja
@@ -109,6 +110,11 @@ public class DefaultControllerAdvice {
 					.collect(Collectors.toList());
 		}
 		return Collections.emptyList();
+	}
+
+	@ExceptionHandler(WebClientRequestException.class)
+	ModelAndView handleException(WebClientRequestException ex) {
+		return errorView("An error occurred on the WebClient request -> " + ex.getMessage());
 	}
 
 	@ExceptionHandler(WebClientResponseException.class)

796aa33d085aabbb7f50b679f17a47bdeeba1ebe
Stefan Ganzer <stefan@innoplexia.com>
2023-01-12 11:53:07 +0100

    Replaces classes by records

--- ui-app/src/main/java/sample/config/ServicesConfig.java
+++ ui-app/src/main/java/sample/config/ServicesConfig.java
@@ -24,37 +24,18 @@ import java.util.Map;
  * @author Stefan Ganzer
  */
 @ConfigurationProperties("oauth2.resource")
-public class ServicesConfig {
+public record ServicesConfig(Map<String, ServiceConfig> services) {
 	public static final String UI_APP = "ui-app";
 	public static final String SERVICE_A = "service-a";
 	public static final String SERVICE_B = "service-b";
-	private Map<String, ServiceConfig> services;
-
-	public Map<String, ServiceConfig> getServices() {
-		return this.services;
-	}
-
-	public void setServices(Map<String, ServiceConfig> services) {
-		this.services = services;
-	}
 
 	public ServiceConfig getConfig(String serviceId) {
-		return this.getServices().entrySet().stream()
+		return services().entrySet().stream()
 				.filter(e -> e.getKey().equalsIgnoreCase(serviceId))
 				.findFirst()
 				.map(Map.Entry::getValue)
 				.orElse(null);
 	}
 
-	public static class ServiceConfig {
-		private String uri;
-
-		public String getUri() {
-			return this.uri;
-		}
-
-		public void setUri(String uri) {
-			this.uri = uri;
-		}
-	}
+  public record ServiceConfig(String uri) {}
 }
